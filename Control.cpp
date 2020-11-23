4239615deab7e930b869961aa92b534b73cec3be
Bryce Horman <47748518+brycehorman@users.noreply.github.com>
2020-11-23 15:50:01 -0500

    Update Control.cpp

--- Control.cpp
+++ Control.cpp
@@ -1,21 +1,68 @@
-#include "Control.h";
+#include "Control.h"
 
-boolean Control::getRegWrite(){return RegWrite;}
-boolean Control::getmemRead(){return memRead;}
-boolean Control::getmemWrite(){return memWrite;}
-boolean Control::getaluSrc(){return aluSrc;}
-boolean Control::getRegDst(){return regDst;}
-boolean Control::getJump(){return jump; }
-boolean Control::getMemToReg(){return memtoReg;}
+using namespace std;
 
-void Control::setALUop()
-{
-  
+// deconstructor
+Control::Control(){
+
 }
+
+// constructor
+Control::~Control(){
+
+}
+
+bool Control::getRegWrite(){return RegWrite;}
+bool Control::getmemRead(){return memRead;}
+bool Control::getmemWrite(){return memWrite;}
+bool Control::getaluSrc(){return aluSrc;}
+bool Control::getRegDst(){return regDst;}
+bool Control::getJump(){return jump; }
+bool Control::getMemToReg(){return memtoReg;}
+string Control::getALUOp(){return ALUop;}
 
 void Control::setValues()
 {
-
+	//from table on page 269 of text
+	//r - format
+	if(opField == "000000"){
+		regDst = true;
+		aluSrc = false;
+		memtoReg = false;
+		RegWrite = true;
+		memRead = false;
+		memWrite = false;
+		jump = false;
+		ALUop = "10";
+	}
+	// lw
+	else if(opField == "100011"){
+		regDst = false;
+		aluSrc = true;
+		memtoReg = true;
+		RegWrite = true;
+		memRead = true;
+		memWrite = false;
+		jump = false;
+		ALUop = "00";
+	}
+	// sw
+	else if(opField == "101011"){
+		aluSrc = true;
+		RegWrite = false;
+		memRead = false;
+		memWrite = true;
+		jump = false;
+		ALUop = "00";
+	}
+	// beq
+	else if(opField == "000100"){
+		aluSrc = false;
+		RegWrite = false;
+		memRead = false;
+		memWrite = false;
+		jump = true;
+		ALUop = "01";
+	}
 
 }
-string getALUOp(){return AlUop;}
