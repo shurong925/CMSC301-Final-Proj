f3054f601fb98152a42b60b155edaaa558afdefb
Bryce Horman <47748518+brycehorman@users.noreply.github.com>
2020-11-29 13:26:08 -0500

    fixed config file reading

--- Processor.cpp
+++ Processor.cpp
@@ -16,51 +16,56 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
   //To open the input file for reading
-  std::ifstream confg(argv[0]);
+  fstream confg;
+  confg.open(argv[1], ios::in);
   //To remind the user that the file cannot be opened
-  if (!confg.is_open())
+  if (!confg)
   {
-      std::cerr << "Error: could not open file: " << argv[1] << std::endl;
-      std::exit(1);
+      cerr << "Error: could not open file: " << argv[1] << endl;
+      exit(1);
   }
 
   string s;
-  vector<string> lines;
+  unordered_map<string, string> config;
   while(getline(confg,s))
   {
-    lines.push_back(s.substring(s.find_first_of(“=”)));
+
+    int equals = s.find("=");
+    if(equals > 0){
+      string key = s.substr(0, equals);
+      string value = s.substr(equals+1);
+      config[key] = value;
+    }
+    
   }
   confg.close();
 
-  // for(int i = 0; i < lines.length(); i++)
-  // {
-  //
-  // }
 
-  string program_input;
-  string memory_contents_input;
-  string register_file_input;
-  string output_mode;
-  string debug;
-  string output_file;
-  string write;
+  string program_input = config["program_input"];
+  string memory_contents_input = config["memory_contents_input"];
+  string register_file_input = config["register_file_input"];
+  string output_mode = config["output_mode"];
+  string debug = config["debug_mode"];
+  string output_file = config["output_file"];
+  string write = config["write_to_file"];
 
-  boolean debug_mode;
-  if(debug = "false")
+  bool debug_mode;
+  if(debug == "false")
     debug_mode = false;
   else
     debug_mode = true;
 
-  boolean write_to_file;
-  if(write = "false")
+  bool write_to_file;
+  if(write == "false")
     write_to_file = false;
   else
     write_to_file = true;
 
   process();
+  return 0;
 }
 
 void process(DataMemory DM, InstructionMemory IM, ProgramCounter PC, RegisterFile RF, Control Control)
