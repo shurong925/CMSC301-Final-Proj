ab78099e29f1bc84b087e14f79810ed1be96c83f
Bryce Horman <47748518+brycehorman@users.noreply.github.com>
2020-11-29 18:41:12 -0500

    Update DataMemory.h

--- DataMemory.h
+++ DataMemory.h
@@ -10,11 +10,11 @@ using namespace std;
 class DataMemory
 {
  private:
-   std:map<string, string> Memory; //Key is address, value is what is stored at that address
+   map<string, string> Memory; //Key is address, value is what is stored at that address
    string address; //The address to read from, from ALU
    string writeData; //The data to be written to memory, from register file
-   boolean memWrite; //Value from control that indicates if the instruction is a write
-    boolean memRead; //Value from control that indicates if the instruction is a read
+   bool memWrite; //Value from control that indicates if the instruction is a write
+    bool memRead; //Value from control that indicates if the instruction is a read
     
  public:
     string read(); //Reads the value from the stored address
