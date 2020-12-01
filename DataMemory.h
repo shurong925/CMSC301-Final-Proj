0eae253b542d04497ab686683e1f699a7c9d139a
Bryce Horman <47748518+brycehorman@users.noreply.github.com>
2020-11-30 19:06:43 -0500

    Update DataMemory.h

--- DataMemory.h
+++ DataMemory.h
@@ -11,12 +11,19 @@ class DataMemory
    unordered_map<string, string> Memory; //Key is address, value is what is stored at that address
    string address; //The address to read from, from ALU
    string writeData; //The data to be written to memory, from register file
+   bool memWrite;
+   bool memRead;
 
 
  public:
     DataMemory(unordered_map<string, string> Memory);
     ~DataMemory();
-    string read(string key); //Reads the value from the stored address
+    string read(); //Reads the value from the stored address
+    void setAddress(string add);
+    void setWriteData(string wd);
+    void setMemWrite(bool mw);
+    void setMemRead(bool mr);
+    void writeTheData();
 
 };
 
